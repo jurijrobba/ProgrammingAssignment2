95b6c3a4c95018a37a14ecd294d4f7e12cb319dc
jurijrobba <robba.jurij@gmail.com>
2016-01-23 11:47:01 +0100

    Assigment done

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+#coursera R programming language course, week 3
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+# make cache matrix function
+# creates object that can store matrix and its inverse
+makeCacheMatrix <- function(x = matrix())
+{
+  #whenever data gets changed, set inverze to null
+  set <- function(y)
+  {
+    x <<- y
+    inv <<- NULL
+  }
+  
+  #get data from object (original matrx)
+  get <- function()
+  {
+    x
+  }
+  
+  #set inverse to inverse of x
+  setinverse <- function(inverse)
+  {
+    inv <<- inverse
+  }
+  
+  #get inverse, if inv is not defined return null from one scope up
+  inv <- NULL
+  getinverse <- function()
+  {
+    inv
+  }
+  
+  #set all values to object
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+#cache solve function, ... are passed to solve function that calculates inverse
+#reads inverse if exists otherwise calculates it and stores it in object
+cacheSolve <- function(x, ...)
+{
+  #if inverse is already calculated dislay message and return it
+  if(!is.null(x$getinverse()))
+  {
+    message("reading from memory")
+    return(x$getinverse())
+  }
+  #else calculate inverse
+  inv <- solve(x$get(), ...)
+  #store it in object
+  x$setinverse(inv)
+  #and return it
+  inv
 }
